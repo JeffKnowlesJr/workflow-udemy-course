f63442b19f5d39e148b402d553d9cc6615154e17
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-06-04 08:53:13 -0400

    Updated webpack config to development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+const path = require("path");
+
 module.exports = {
   entry: "./scripts/App.js",
+  output: {
+    filename: "workflow.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  mode: "development",
+  // Why is my linter creating trailing commas?
 };
