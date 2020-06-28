2b16fcb5dd278881bf8b97902f07b311055ef91e
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-06-27 21:56:15 -0400

    Finished 13 OOP

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,3 +4,43 @@ import "../styles/styles.css";
 if (module.hot) {
   module.hot.accept();
 }
+
+// Lesson example code below this line
+// Nouns (Properties) and verbs (Methods)
+
+// let jeff = {
+//   name: "Jeff Knowles",
+//   favoriteColor: "green",
+//   greet: function () {
+//     console.log(
+//       "Hello, my name is " +
+//         this.name +
+//         " and my favorite color is " +
+//         this.favoriteColor +
+//         "."
+//     );
+//   },
+// };
+
+// jeff.greet();
+
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.color = favColor;
+
+  this.greet = function () {
+    console.log(
+      "Hello, my name is " +
+        this.name +
+        " and my favorite color is " +
+        this.color +
+        "."
+    );
+  };
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "orange");
+jane.greet();
